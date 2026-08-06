b7a30462350643c40e1741c96bf4fab93ffd1dfe
INTJcodes <lolarosegooch@gmail.com>
2026-08-06 19:43:01 +0100

    Rate limiting and lists in forms

---
+++ src/public/js/insertFormListItem.js
@@ -0,0 +1,73 @@
+export class insertItem {
+    constructor() {
+        this.addToSymptom = document.getElementById("symptoms");
+        this.addToAssociation = document.getElementById("associatedFoods");
+        this.count = 0;
+    }
+
+    createDeletor(newDiv) {
+        // Used to delete the newly added list element
+        var deleterElementDiv = document.createElement("div");
+        deleterElementDiv.setAttribute("class", "icon-parent h-2");
+
+        deleterElementDiv.addEventListener("click", (e)=>{
+            e.target.parentElement.parentElement.remove();
+        })
+
+        var deleterElementImg = document.createElement("img");
+        deleterElementImg.setAttribute("src", "./assets/plain.png");
+        deleterElementImg.setAttribute("alt", "A bin icon to delete an item");
+        deleterElementImg.setAttribute("class", "icon");
+        deleterElementImg.setAttribute("style", "mask-image: url('./assets/Delete-Icon.png')");
+
+        deleterElementDiv.appendChild(deleterElementImg);
+        newDiv.appendChild(deleterElementDiv);
+    }
+
+    listenForSymptomAdd(elementId) {
+        var target = document.getElementById(elementId);
+        
+        target.addEventListener("click", ()=>{
+            var newDiv = document.createElement("div");
+
+            var newInputElement = document.createElement("input");
+            newInputElement.setAttribute("type", "text");
+            newInputElement.setAttribute("name", "symptom"+this.count);
+            this.count++;
+            newInputElement.setAttribute("class", "border-underline");
+            newDiv.appendChild(newInputElement);
+
+            this.createDeletor(newDiv);
+
+            this.addToSymptom.appendChild(newDiv);
+        })
+        return;
+    }
+
+    listenForAssociationAdd(elementId) {
+        var target = document.getElementById(elementId);
+        
+        target.addEventListener("click", ()=>{
+            var newDiv = document.createElement("div");
+
+            var newIcon = document.createElement("img");
+            newIcon.setAttribute("src", "./assets/plain.png");
+            newIcon.setAttribute("alt", "An icon of a strawberry, linking to the create food page");
+            newIcon.setAttribute("class", "icon h-2");
+            newIcon.setAttribute("style", "mask-image: url('./assets/food-icon.png')");
+            newDiv.appendChild(newIcon);
+
+            var newInputElement = document.createElement("input");
+            newInputElement.setAttribute("type", "text");
+            newInputElement.setAttribute("name", "symptom"+this.count);
+            this.count++;
+            newInputElement.setAttribute("class", "border-underline");
+            newDiv.appendChild(newInputElement);
+
+            this.createDeletor(newDiv);
+
+            this.addToAssociation.appendChild(newDiv);
+        })
+        return;
+    }
+}
--- src/routes/create-sickness.js
+++ src/routes/create-sickness.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+    max: 3, // Max no. of requests
+    windowMs: 5*60*1000, // Rate limit time (ms)
+    message: 'Too many attempts. Please try again later.',
+    handler: (req, res, next, options) => {res.render('blocked', {blocked:true})}
+});
+
 const createModel = require('../model/createModel');
 
 router.get('/', async function(req, res) {
@@ -17,6 +25,14 @@ router.get('/', async function(req, res) {
         console.error(error);
         res.render('error');
     }
+});
+
+router.post("/", limiter, async(req, res, next) => {
+    const name = req.body.name;
+    const time = req.body.time;
+    const description = req.body.description;
+    // Get symptoms
+    // Get associated foods
 });
 
 module.exports = router;
